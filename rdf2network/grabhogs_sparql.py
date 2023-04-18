e8abc69335125b200af5cc253a0160e91ab8f1ff
cactuskid <cactuskid13@gmail.com>
2023-04-18 16:52:16 +0200

    refactoring

--- rdf2network/grabhogs_sparql.py
+++ rdf2network/grabhogs_sparql.py
@@ -1,71 +1,104 @@
 import time, traceback
 import sys
-from SPARQLWrapper import SPARQLWrapper, JSON
+from SPARQLWrapper import SPARQLWrapper, JSON , JSONLD
+from rdflib import Graph, URIRef
+import json
+from rdflib.plugins.sparql.results.jsonresults import JSONResultParser
+import io
 import rdflib
 import sys, os, time
 import pandas as pd
 import stringdb
 
-
 # always display full column results (don't truncate output)
 pd.set_option('display.max_colwidth', None)
-
 # just for reference, check how many of the proteins require multiple batch calls
 over_limits = 0
 
-# function to print in a table results of a SPARQL query
+# this is the batch size we want for SPARQL queries
+LIMIT = 10000
+
+
 def results_to_pandas(results):
-    
+
     # how to transform SPARQL results into Pandas dataframes
     if(len(results["results"]["bindings"]) == 0):
         # this might actually correspond to an error since
         # at least the header (row 0) should be returned
         return pd.DataFrame()
-    
     # get header (column names) from results
     header = results["results"]["bindings"][0].keys()
-
     # display table of results:
     table = []
-    
     # the SPARQL JSON results to the query are available in the "results", "bindings" entry:
     for entry in results["results"]["bindings"]:
         # append entries from the results to a regular Python list of rows, which we can then transform to a Pandas DF
         row = [entry[column]["value"] if entry.get(column, None) != None else None for column in header]
         table.append(row)
+        #add the entry to rdflib graph
     df = pd.DataFrame(table, columns=list(header))
-    return df
+    return df 
+
+    
+def get_string_id(uniprot_url, taxon_url):
+    accession = uniprot_url.rsplit('/', 1)[1]
+    taxon_id = taxon_url.rsplit('/', 1)[1]
+    if(uniprot_url not in string_ids_dict):
+    # NOTE! Here if no species mentioned, defaults to HUMAN
+    # also, if protein not available in STRING, it will through a ValueException error
+        try:
+            string_ids_dict[uniprot_url] = stringdb.get_string_ids([accession], int(taxon_id))["stringId"]
+        except:
+            pass
+
+
 
 # this is the batch size we want for SPARQL queries
 LIMIT = 10000
+def results_to_pandas(results):
+    # how to transform SPARQL results into Pandas dataframes
+    if(len(results["results"]["bindings"]) == 0):
+        # this might actually correspond to an error since
+        # at least the header (row 0) should be returned
+        return pd.DataFrame()
+    # get header (column names) from results
+    header = results["results"]["bindings"][0].keys()
+    # display table of results:
+    table = []
+    # the SPARQL JSON results to the query are available in the "results", "bindings" entry:
+    for entry in results["results"]["bindings"]:
+        # append entries from the results to a regular Python list of rows, which we can then transform to a Pandas DF
+        row = [entry[column]["value"] if entry.get(column, None) != None else None for column in header]
+        table.append(row)
+        #add the entry to rdflib graph
+    df = pd.DataFrame(table, columns=list(header))
+    return df 
 
-def query_SPARQL_with_limit_offset(sparql_query, limit, offset, sparql_endpoint):
+def query_SPARQL_with_limit_offset(sparql_query, limit, offset, sparql_endpoint , fmt = JSON):
     global over_limits
-    
     query_with_limit_offset = f'{sparql_query} LIMIT {limit} OFFSET {offset}'
-    
     #print(query_with_limit_offset)
-    
     if(sparql_endpoint is not None):
         sparql_endpoint.setQuery(query_with_limit_offset)
-        sparql_endpoint.setReturnFormat(JSON)
-
+        sparql_endpoint.setReturnFormat(fmt)
         results_OMA = sparql_endpoint.query().convert()
-
     else:
         print("Error: Configure SPARQL Endpoint")
-        return None
-    
-    if(len(results_OMA["results"]["bindings"]) == LIMIT):
-        # Request next set of results until end, append to previous
-        if(offset == 0):
-            over_limits += 1
-            #print("Over 10K results for {}".format(results_OMA["results"]["bindings"][1]))
-        return pd.concat([results_to_pandas(results_OMA), query_SPARQL_with_limit_offset(sparql_query, LIMIT, offset + LIMIT, sparql_endpoint)])
-   
-    return results_to_pandas(results_OMA)
+        return None    
+    import pdb ; pdb.set_trace()
 
-def query_orthologs(uniprot_entry, sparql_endpoint):
+    if fmt == JSON  and (len(results_OMA["results"]["bindings"]) == LIMIT):
+        # Request next set of results until end, append to previous
+        res = results_OMA["results"]["bindings"]
+        while len(res) == LIMIT and offset < 5*LIMIT:
+            #print("Over 10K results for {}".format(results_OMA["results"]["bindings"][1]))    
+            additional_bindings =  query_SPARQL_with_limit_offset(sparql_query, offset , offset + LIMIT, sparql_endpoint )
+            offset += LIMIT
+            results_OMA["results"]["bindings"] += additional_bindings["results"]["bindings"]
+            res = additional_bindings["results"]["bindings"]
+    return results_OMA
+
+def query_orthologs(uniprot_entry, sparql_endpoint , return_graph = False):
 
     offset = 0
     
@@ -94,11 +127,19 @@ def query_orthologs(uniprot_entry, sparql_endpoint):
     }  
     """
     
-    results_OMA = query_SPARQL_with_limit_offset(query_OMA_orthologs, LIMIT, offset, sparql_endpoint)
-    
-    return results_OMA
+    if return_graph == True:
+        fmt = JSONLD
+        data = query_SPARQL_with_limit_offset(query_OMA_orthologs, LIMIT, offset, sparql_endpoint , fmt= fmt)
+        graph = Graph()
+        graph.parse(data=json.dumps(data), format='json-ld')
+        return  graph
+    else:       
+        fmt = JSON
+        results_OMA = query_SPARQL_with_limit_offset(query_OMA_orthologs, LIMIT, offset, sparql_endpoint)
+        results_OMA = results_to_pandas(results_OMA)
+        return results_OMA
         
-def query_paralogs(uniprot_entry, sparql_endpoint):
+def query_paralogs(uniprot_entry, sparql_endpoint , return_graph = False):
     
     offset = 0
     
@@ -126,49 +167,128 @@ def query_paralogs(uniprot_entry, sparql_endpoint):
         FILTER(?para_node1 != ?para_node2)
     }
     """
-    
-    results_OMA = query_SPARQL_with_limit_offset(query_OMA_paralogs, LIMIT, offset, sparql_endpoint)
-    
-    return results_OMA
-    
-def get_string_id(uniprot_url, taxon_url):
-    accession = uniprot_url.rsplit('/', 1)[1]
-    taxon_id = taxon_url.rsplit('/', 1)[1]
 
-    if(uniprot_url not in string_ids_dict):
-    # NOTE! Here if no species mentioned, defaults to HUMAN
-    # also, if protein not available in STRING, it will through a ValueException error
-        try:
-            string_ids_dict[uniprot_url] = stringdb.get_string_ids([accession], int(taxon_id))["stringId"]
-        except:
-            pass
-
-# HERE, get HOGS for all vertices in G from oma sparql endpoint
-
-def grab_hogs( G , subg , cross_ref= rdflib.term.URIRef("http://purl.org/lscr#xrefUniprot") , sparql_endpoint= None):
+    if return_graph == True:
+        fmt = JSONLD
+        data = query_SPARQL_with_limit_offset(query_OMA_paralogs, LIMIT, offset, sparql_endpoint , fmt= fmt)
+        graph = Graph()
+        graph.parse(data=json.dumps(data), format='json-ld')
+        return  graph
+    else:       
+        fmt = JSON
+        results_OMA = query_SPARQL_with_limit_offset(query_OMA_paralogs, LIMIT, offset, sparql_endpoint)
+        results_OMA = results_to_pandas(results_OMA)
+        return results_OMA
+"""
+def grab_hogs_graph( subg , cross_ref , sparql_endpoint= None , USE_CASE = 1 , verbose = True , cross_ref_prop = rdflib.term.URIRef("http://purl.org/lscr#xrefUniprot")):
     start_time = time.time()
-
     num_queries = 0
     num_errors = 0
-
     global over_limits 
     over_limits = 0
-
     proteins_by_species = {}
     # FIRST, need to get UniProt IDs for all string prots needed
-
-    for i,e in enumerate(G.edges(data = True , keys=True)):
-        uri_subj = e[0]
-        uri_obj = e[1]
-        
-        cross_ref_prop = rdflib.term.URIRef("http://purl.org/lscr#xrefUniprot")
-        
-        cross_ref_subj = [o for (s,p,o) in subg.triples((uri_subj, cross_ref, None))][0]
-        cross_ref_obj = [o for (s,p,o) in subg.triples((uri_obj, cross_ref, None))][0]
-        
+    subjs = [[o for (s,p,o) in subg.triples((None, cross_ref, None))]]
+    subjs = [ s[0] for s in subjs if len(s) > 0 ]
+    subjs = set(subjs)
+    print(len(subjs))
+    orthograph = Graph()
+    for cross_ref_subj in subjs:
+        # get cross reference for interacting protein 1, cross_ref_subj                
         # options: 1. UNIL OMA endpoint; 2. official OMA endpoint;
-        
-        USE_CASE = 1
+        if(USE_CASE == 1):
+            sparql_OMA = SPARQLWrapper("https://unil.omabrowser.org/sparql/")
+        elif(USE_CASE == 2):
+            sparql_OMA = SPARQLWrapper("https://sparql.omabrowser.org/sparql/")
+        else:
+            sparql_OMA = None
+        # get HOG of interacting protein 1, cross_ref_subj (orthologous proteins + species)
+        cross_ref_subj = "<" + cross_ref_subj + ">"  
+        #print("Getting HOGs of " + cross_ref_subj)
+        # try querying the remote server but skip over errors if any
+        # NOTE: it seems like errors happen when number of results is too big,
+        # e.g. <http://purl.uniprot.org/uniprot/B6JXK8> seems to have 55K paralogs!?
+        try:
+            g1 = query_orthologs(cross_ref_subj, sparql_OMA , return_graph=True )
+            orthograph += g1
+        except Exception:
+            traceback.print_exc()
+            num_errors += 1
+            pass
+        try:
+            results_subj_para , g1 = query_paralogs(cross_ref_subj, sparql_OMA , return_graph = True)
+            if("para_protein_uniprot" in results_subj_para.columns):
+                df1_grouped = results_subj_para[["para_protein_uniprot","taxon_para"]].groupby('taxon_para')
+                orthograph += g1
+        except Exception:
+            #traceback.print_exc()
+            num_errors += 1
+            pass
+        #results_to_pandas(results_obj)
+        num_queries += 2
+    end_time = time.time()
+    if verbose == True:
+        print("Total time for {} SPARQL queries: {} seconds (multiple batch calls in: {} cases)".format(num_queries, end_time - start_time, over_limits))
+        print("Num errors: {}".format(num_errors)) 
+    return orthograph
+"""
+
+
+def grab_hogs_graph( subg , cross_ref , sparql_endpoint= None , USE_CASE = 1 , verbose = True , cross_ref_prop = rdflib.term.URIRef("http://purl.org/lscr#xrefUniprot")):
+    start_time = time.time()
+    num_queries = 0
+    num_errors = 0
+    global over_limits 
+    over_limits = 0
+    proteins_by_species = {}
+    # FIRST, need to get UniProt IDs for all string prots needed
+    subjs = [[o for (s,p,o) in subg.triples((None, cross_ref, None))]]
+    subjs = [ s[0] for s in subjs if len(s) > 0 ]
+    subjs = set(subjs)
+    print(len(subjs))
+    orthograph = Graph()
+    for cross_ref_subj in subjs:
+        # get cross reference for interacting protein 1, cross_ref_subj                
+        # options: 1. UNIL OMA endpoint; 2. official OMA endpoint;
+        if(USE_CASE == 1):
+            sparql_OMA = SPARQLWrapper("https://unil.omabrowser.org/sparql/")
+        elif(USE_CASE == 2):
+            sparql_OMA = SPARQLWrapper("https://sparql.omabrowser.org/sparql/")
+        else:
+            sparql_OMA = None
+        # get HOG of interacting protein 1, cross_ref_subj (orthologous proteins + species)
+        cross_ref_subj = "<" + cross_ref_subj + ">"  
+        #print("Getting HOGs of " + cross_ref_subj)
+        # try querying the remote server but skip over errors if any
+        # NOTE: it seems like errors happen when number of results is too big,
+        # e.g. <http://purl.uniprot.org/uniprot/B6JXK8> seems to have 55K paralogs!?
+        g1 = query_orthologs(cross_ref_subj, sparql_OMA , return_graph=True )
+        orthograph += g1
+        g1 = query_paralogs(cross_ref_subj, sparql_OMA , return_graph = True)
+        orthograph += g1
+        #results_to_pandas(results_obj)
+        num_queries += 2
+    end_time = time.time()
+    if verbose == True:
+        print("Total time for {} SPARQL queries: {} seconds (multiple batch calls in: {} cases)".format(num_queries, end_time - start_time, over_limits))
+        print("Num errors: {}".format(num_errors)) 
+    return orthograph
+
+#get the OMA orthology info for the string subgraph
+def grab_hogs( subg , cross_ref , sparql_endpoint= None , USE_CASE = 1 , verbose = True , cross_ref_prop = rdflib.term.URIRef("http://purl.org/lscr#xrefUniprot")):
+    start_time = time.time()
+    num_queries = 0
+    num_errors = 0
+    global over_limits 
+    over_limits = 0
+    proteins_by_species = {}
+    # FIRST, need to get UniProt IDs for all string prots needed
+    subjs = [ o for (s,p,o) in subg.triples((None, cross_ref, None))]
+    subjs = set(subjs)
+    print(len(subjs))
+    for cross_ref_subj in subjs:
+        # get cross reference for interacting protein 1, cross_ref_subj                
+        # options: 1. UNIL OMA endpoint; 2. official OMA endpoint;
         
         if(USE_CASE == 1):
             sparql_OMA = SPARQLWrapper("https://unil.omabrowser.org/sparql/")
@@ -176,48 +296,38 @@ def grab_hogs( G , subg , cross_ref= rdflib.term.URIRef("http://purl.org/lscr#xr
             sparql_OMA = SPARQLWrapper("https://sparql.omabrowser.org/sparql/")
         else:
             sparql_OMA = None
-        
         # get HOG of interacting protein 1, cross_ref_subj (orthologous proteins + species)
         cross_ref_subj = "<" + cross_ref_subj + ">"  
         #print("Getting HOGs of " + cross_ref_subj)
-        
         # try querying the remote server but skip over errors if any
         # NOTE: it seems like errors happen when number of results is too big,
         # e.g. <http://purl.uniprot.org/uniprot/B6JXK8> seems to have 55K paralogs!?
         try:
-            results_subj_ortho = query_orthologs(cross_ref_subj, sparql_OMA)
-            
+            results_subj_ortho  = query_orthologs(cross_ref_subj, sparql_OMA, return_graph = False  )
             # fill dictionary with accessions to look for by taxon_id
             # this will be used to query STRING API (batched by species)
             if("orth_protein_uniprot" in results_subj_ortho.columns):
                 df1_grouped = results_subj_ortho[["orth_protein_uniprot","taxon_orth"]].groupby('taxon_orth')
-        
                 # iterate over each group
                 for group_name, df_group in df1_grouped:
-            
                     taxon_id = group_name.rsplit('/', 1)[1]
                     if(proteins_by_species.get(taxon_id) == None):
                         proteins_by_species[taxon_id] = set()
                     #print('group {}'.format(taxon_id))
-            
                     for row_index, row in df_group.iterrows():
                         protein_url = row["orth_protein_uniprot"]
                         accession = protein_url.rsplit('/', 1)[1]
                         proteins_by_species[taxon_id].add(accession)
-                    
         except Exception:
             traceback.print_exc()
             num_errors += 1
             pass
         try:
-            results_subj_para = query_paralogs(cross_ref_subj, sparql_OMA)
-            
+            results_subj_para  = query_paralogs(cross_ref_subj, sparql_OMA , return_graph = False)
             if("para_protein_uniprot" in results_subj_para.columns):
                 df1_grouped = results_subj_para[["para_protein_uniprot","taxon_para"]].groupby('taxon_para')
-        
                 # iterate over each group
                 for group_name, df_group in df1_grouped:
-            
                     taxon_id = group_name.rsplit('/', 1)[1]
                     if(proteins_by_species.get(taxon_id) == None):
                         proteins_by_species[taxon_id] = set() 
@@ -227,63 +337,14 @@ def grab_hogs( G , subg , cross_ref= rdflib.term.URIRef("http://purl.org/lscr#xr
                         protein_url = row["para_protein_uniprot"]
                         accession = protein_url.rsplit('/', 1)[1]
                         proteins_by_species[taxon_id].add(accession)
-                
-        except Exception:
-            #traceback.print_exc()
-            num_errors += 1
-            pass
-        
-        # get HOG of interacting protein 2, cross_ref_obj (orthologous proteins + species )
-        
-        #print("Getting HOGs of " + cross_ref_obj)
-        cross_ref_obj = "<" + cross_ref_obj + ">"    
-        try:
-            results_obj_ortho = query_orthologs(cross_ref_obj, sparql_OMA)
-            if("orth_protein_uniprot" in results_obj_ortho.columns):
-                df1_grouped = results_obj_ortho[["orth_protein_uniprot","taxon_orth"]].groupby('taxon_orth')
-
-                # iterate over each group
-                for group_name, df_group in df1_grouped:
-        
-                    taxon_id = group_name.rsplit('/', 1)[1]
-                    if(proteins_by_species.get(taxon_id) == None):
-                        proteins_by_species[taxon_id] = set() 
-                    #print('group {}'.format(taxon_id))
-        
-                    for row_index, row in df_group.iterrows():
-                        protein_url = row["orth_protein_uniprot"]
-                        accession = protein_url.rsplit('/', 1)[1]
-                        proteins_by_species[taxon_id].add(accession)
-                
-        except Exception:
-            #traceback.print_exc()
-            num_errors += 1
-            pass
-        
-        try:
-            results_obj_para = query_paralogs(cross_ref_obj, sparql_OMA)
-
-            if("para_protein_uniprot" in results_obj_para.columns):
-                df1_grouped = results_obj_para[["para_protein_uniprot","taxon_para"]].groupby('taxon_para')
-
-                # iterate over each group
-                for group_name, df_group in df1_grouped:
-                    taxon_id = group_name.rsplit('/', 1)[1]
-                    if(proteins_by_species.get(taxon_id) == None):
-                        proteins_by_species[taxon_id] = set()
-            
-                    for row_index, row in df_group.iterrows():
-                        protein_url = row["para_protein_uniprot"]
-                        accession = protein_url.rsplit('/', 1)[1]
-                        proteins_by_species[taxon_id].add(accession)
-                
         except Exception:
             #traceback.print_exc()
             num_errors += 1
             pass
         #results_to_pandas(results_obj)
-        num_queries += 4
+        num_queries += 2
     end_time = time.time()
-    print("Total time for {} SPARQL queries: {} seconds (multiple batch calls in: {} cases)".format(num_queries, end_time - start_time, over_limits))
-    print("Num errors: {}".format(num_errors)) 
-    return proteins_by_species , results_subj_para , results_subj_ortho , results_obj_para , results_obj_ortho
+    if verbose == True:
+        print("Total time for {} SPARQL queries: {} seconds (multiple batch calls in: {} cases)".format(num_queries, end_time - start_time, over_limits))
+        print("Num errors: {}".format(num_errors)) 
+    return proteins_by_species , results_subj_para , results_subj_ortho 
--- rdf2network/sampler.py
+++ rdf2network/sampler.py
@@ -21,44 +21,53 @@ def load_graph(datapath):
         return rg
     
 #recursive function to get some neighbours
-def grab_neighbours(seed,rg, limit = 10, recur = 0 , maxrecur = 3 , sample_run = 100 ):
+def grab_neighbours(seed,rg, limit = 10, recur = 0 , maxrecur = 3 , sample_run = 10 ):
     nodes = []
     #sample the neighbourhood randomly
-    objs =  rg.objects(subject = seed ) 
-    objs = [  next(objs) for i in range(sample_run) ]
+    obj_gen =  rg.objects(subject = seed )
+    objs = []
+    subgraph = None
+    for obj in obj_gen:
+        triples = [ t for t in rg.triples((obj,None,seed))]
+        nodes.append(obj)
+        if subgraph is None:
+            subgraph = triples
+        else:
+            subgraph += triples
+        objs.append(obj)
+        if len( objs) > sample_run-1:
+            break
     random.shuffle(objs)
     for i,obj in enumerate(objs):
         if i < limit and recur < maxrecur:
-            nodes += grab_neighbours(obj , rg,limit = limit, recur = recur+1 )    
+            subnodes, subg = grab_neighbours(obj,rg,limit = limit, recur = recur+1 )    
+            nodes += subnodes
+            subgraph += subg
         elif i < limit:
             nodes.append( obj )
+            subgraph += [ t for t in rg.triples((obj,None,seed))]
         if i > limit:
             break
-    return nodes
+    return nodes, subgraph
 
-def sample( rg , subject_iter = None , seed = None, sample_run= 10,  layers = 3 , layer_limit = 3 , verbose = True):
-    if not seed:
-        seed = next(subject_iter)
-    if verbose == True:
-        print('seed', seed)
-    nodes = grab_neighbours(seed,rg, limit = layer_limit, recur = 0 , sample_run=sample_run, maxrecur = layers)
+def sample( rg  , seed , sample_run= 10, limit = 5,  layers = 3 , layer_limit = 3 , verbose = True):
+    nodes , subgraph = grab_neighbours(seed,rg, limit = layer_limit , recur = 0 , sample_run=sample_run, maxrecur = layers)
+    print(len(nodes) , len(subgraph))
     subG = RDFGraph()
-    triples = [ rg.triples((n1,None,n2)) for n1 in nodes for n2 in nodes if n1 != n2 ]
-    for t in triples:
-        subG+=t
+    [   subG.add( (s,p,o) ) for s,p,o in subgraph ]
     return subG
 
 def add_xrefs(rg_info, subg):
-    subg += rg_info
     print(subg)
-    cross_ref = rdflib.term.URIRef("http://purl.org/lscr#xrefUniprot")
+    cross_ref = rdflib.term.URIRef('http://purl.org/lscr#xrefUniprot')
     string_ids_dict = {}
     # put in dictionary the IDs of original proteins (for which we got orthologs and paralogs)
-    cross_ref = rdflib.term.URIRef("http://purl.org/lscr#xrefUniprot")
-    for (s,p,o) in subg.triples((None, cross_ref, None)):
-        # the triple is in the form (STRING_ID, xrefUniprot, UNIPROT_ID)
-        uniprot_url = str(o)
-        string_url = str(s)
-        if(uniprot_url not in string_ids_dict):
-            string_ids_dict[uniprot_url] = string_url
-    return string_ids_dict , subg
+    #iterate over all the subjects and objects in subg
+    prots = set([ o for o in subg.objects() ] ).union(
+        set([ s for s in subg.subjects() ] )
+    )
+    print(len(prots))
+    triples = [(s,p,o)  for p in prots for s,p,o in rg_info.triples((p, cross_ref, None)) ]
+    print(len(triples))
+    [ subg.add(t) for t in triples ]
+    return subg
